e1cd0bf5676c11634b9aab80a5b8592a1a6df860
Monk <forgetmenaught@gmail.com>
2024-07-10 16:29:25 -0700

    Added other config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true'
+});
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production'
+    }
+};
+
+export default withBundleAnalyzer(nextConfig);
