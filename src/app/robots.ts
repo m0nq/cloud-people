45b349328f8db4db840d6c7b167dab555372c6b5
Monk <forgetmenaught@gmail.com>
2024-07-31 17:06:06 -0700

    Added robots file

---
+++ src/app/robots.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from 'next';
+
+const robots = (): MetadataRoute.Robots => ({
+    rules: [
+        {
+            userAgent: '*',
+            disallow: '/'
+        },
+        {
+            userAgent: [
+                'baiduspider',
+                'baiduspider-image',
+                'baiduspider-mobile',
+                'baiduspider-news',
+                'baiduspider-video',
+                'bingbot',
+                'msnbot',
+                'msnbot-media',
+                'adidxbot',
+                'Googlebot',
+                'Googlebot-Image',
+                'Googlebot-Mobile',
+                'Googlebot-News',
+                'Googlebot-Video',
+                'Storebot-Google',
+                'Mediapartners-Google',
+                'AdsBot-Google',
+                'slurp',
+                'yandex'
+            ],
+            allow: '/',
+            disallow: ['/community', '/dashboard', '/profile', '/sandbox', '/store']
+        }
+    ]
+});
+
+export default robots;
