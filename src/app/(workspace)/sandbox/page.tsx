c95995b7d1fc4873795bfdb1298df90edefcc114
Monk <forgetmenaught@gmail.com>
2024-08-20 18:54:33 -0700

    First set button icons with their styles

--- src/app/(workspace)/sandbox/page.tsx
+++ src/app/(workspace)/sandbox/page.tsx
@@ -12,7 +12,11 @@ import { Panel } from '@xyflow/react';
 import { ControlButton } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { PiPlayCircleThin } from 'react-icons/pi';
+import { PiClipboardLight } from 'react-icons/pi';
 import { CiSearch } from 'react-icons/ci';
+import { CiCircleCheck } from 'react-icons/ci';
+import { FiUserPlus } from 'react-icons/fi';
+import { IoHandRightOutline } from 'react-icons/io5';
 
 import './automation.styles.css';
 import { AutomationNode } from './automation-node';
@@ -93,19 +97,30 @@ const Sandbox = () => {
                         Button
                     </ControlButton>
                     <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                        Button
+                        <IoHandRightOutline className="icon-button hand-icon" />
                     </ControlButton>
                     <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                        Button
+                        <FiUserPlus className="icon-button add-agent-icon" strokeWidth={1.1} />
                     </ControlButton>
                     <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                        Button
+                        <svg className="icon-button branches-icon"
+                            strokeWidth="1.1"
+                            viewBox="4 -4 26 26"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <rect x="16.8995" y="1" width="14" height="14" transform="rotate(45 16.8995 1)"
+                                stroke="#9CA3AF" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M1 11L7 11" stroke="#9CA3AF" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M22 5.65686L26.2426 1.41422" stroke="#9CA3AF" strokeLinecap="round"
+                                strokeLinejoin="round" />
+                            <path d="M22 16L26.2426 20.2426" stroke="#9CA3AF" strokeLinecap="round"
+                                strokeLinejoin="round" />
+                        </svg>
                     </ControlButton>
                     <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                        Button
+                        <PiClipboardLight className="icon-button" />
                     </ControlButton>
                     <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                        Button
+                        <CiCircleCheck className="icon-button" />
                     </ControlButton>
                 </Controls>
                 <Controls position="top-center"
