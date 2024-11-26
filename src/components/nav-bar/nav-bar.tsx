2a6afc8e927dbb089bdfc79ed247f7329d7988da
Monk <forgetmenaught@gmail.com>
2024-11-25 16:09:19 -0800

    Added nav bar toggle on mouse enter/leave events

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useTransition } from 'react';
+import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 // import Image from 'next/image';
@@ -9,6 +10,7 @@ import { PiSignOutLight } from 'react-icons/pi';
 import { PiYoutubeLogoLight } from 'react-icons/pi';
 import { PiQuestionLight } from 'react-icons/pi';
 import { BiMenuAltLeft } from 'react-icons/bi';
+import { BsArrowLeftShort } from 'react-icons/bs';
 
 import './nav-bar.styles.css';
 import { signOut } from '@lib/actions/authentication-actions';
@@ -77,44 +79,59 @@ const secondaryLinks = [
 export const NavBar = () => {
     const pathname = usePathname();
     const [isPending, startTransition] = useTransition();
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsNavVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsNavVisible(false);
+    };
 
     return (
-        <nav className="navigation-area">
-            <div className="navigation-bar-top">
-                <ul className="navigation-primary">
-                    {primaryLinks.map(({ iconElement, link, name }) => {
-                        const linkedPath = pathname === link;
-                        return (
-                            <li key={name} className={linkedPath && 'nav-marker' || ''}>
-                                <Link href={link}>
-                                    {iconElement(linkedPath && '#502dff' || 'currentColor')}
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
+        <div className="nav-bar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className={`toggle-button ${isNavVisible ? 'hidden' : ''}`}>
+                <BsArrowLeftShort />
             </div>
-            <div className="navigation-bar-bottom">
-                <ul className="navigation-secondary">
-                    {secondaryLinks.map(({ iconElement, link, name }) => {
-                        const linkedPath = pathname === link;
-                        return (
-                            <li key={name} className={linkedPath && 'nav-marker' || ''}>
-                                <Link href={link}>
-                                    {iconElement(linkedPath && '#502dff' || 'currentColor')}
-                                </Link>
-                            </li>
-                        );
-                    })}
-                    <li key="sign-out">
-                        <button className="sign-out"
-                            onClick={() => startTransition(async () => await signOut())}
-                            disabled={isPending}>
-                            <PiSignOutLight />
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+
+            <nav className={`navigation-area ${!isNavVisible ? 'hidden' : ''}`}>
+                <div className="navigation-bar-top">
+                    <ul className="navigation-primary">
+                        {primaryLinks.map(({ iconElement, link, name }) => {
+                            const linkedPath = pathname === link;
+                            return (
+                                <li key={name} className={linkedPath && 'nav-marker' || ''}>
+                                    <Link href={link}>
+                                        {iconElement(linkedPath && '#502dff' || 'currentColor')}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className="navigation-bar-bottom">
+                    <ul className="navigation-secondary">
+                        {secondaryLinks.map(({ iconElement, link, name }) => {
+                            const linkedPath = pathname === link;
+                            return (
+                                <li key={name} className={linkedPath && 'nav-marker' || ''}>
+                                    <Link href={link}>
+                                        {iconElement(linkedPath && '#502dff' || 'currentColor')}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                        <li key="sign-out">
+                            <button className="sign-out"
+                                onClick={() => startTransition(async () => await signOut())}
+                                disabled={isPending}>
+                                <PiSignOutLight />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     );
 };
