5d4536e1a44c8a28bc1fa87a6da290d9c31d709b
Monk <forgetmenaught@gmail.com>
2024-11-26 12:18:25 -0800

    Refactored colors to tailwind variables.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,10 +16,45 @@ const config: Config = {
         },
         extend: {
             colors: {
+                // Brand colors
                 primary: '#6652ff',
                 secondary: '#56e8cd',
+                
+                // Navigation
+                'nav-border': '#3D3D3D',
+                'nav-bg': '#181A1A',
+                'nav-text': '#bac5d1',
+                
+                // Text colors
+                'text-primary': '#515568',
+                'text-secondary': '#BEC1CF',
+                'text-meta': '#9D9D9D',
+                'text-light': '#E1E1EE',
+                
+                // UI Elements
                 'icon-gray': '#818181',
-                dark: '#131414'
+                'border-light': '#DBDBDB',
+                dark: '#131414',
+                
+                // Login specific
+                'login-bg': '#56E8CD',
+                'login-text': '#18642D',
+                'login-button': '#614CFF',
+                'login-button-hover': 'rgba(97, 76, 255, 0.4)',
+                
+                // Gradients (to be used with bg-gradient-to-r)
+                'gradient-purple': {
+                    from: '#A852FF',
+                    to: '#6A52FF'
+                },
+                'gradient-teal': {
+                    from: '#86FFE2',
+                    to: '#18FFD5'
+                },
+                'gradient-blue': {
+                    from: '#868CFF',
+                    to: '#4318FF'
+                }
             }
         }
     },
