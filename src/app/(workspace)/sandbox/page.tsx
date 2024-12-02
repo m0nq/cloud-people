e17fecc91dd3f52fee80a362f4e11b56adddfa53
Monk <forgetmenaught@gmail.com>
2024-12-02 15:48:41 -0800

    Simplified styles for sandbox controller buttons

--- src/app/(workspace)/sandbox/page.tsx
+++ src/app/(workspace)/sandbox/page.tsx
@@ -1,16 +1,14 @@
 'use client';
 import { ReactNode } from 'react';
 import '@xyflow/react/dist/style.css';
-import { Controls } from '@xyflow/react';
 import { Panel } from '@xyflow/react';
-import { ControlButton } from '@xyflow/react';
 import { ReactFlow } from '@xyflow/react';
-import { PiClipboardLight } from 'react-icons/pi';
-import { CiSearch } from 'react-icons/ci';
-import { CiCircleCheck } from 'react-icons/ci';
-import { FiUserPlus } from 'react-icons/fi';
-import { IoHandRightOutline } from 'react-icons/io5';
 import { LuMousePointer } from 'react-icons/lu';
+import { IoHandRightOutline } from 'react-icons/io5';
+import { FiUserPlus } from 'react-icons/fi';
+import { PiClipboardLight } from 'react-icons/pi';
+import { CiCircleCheck } from 'react-icons/ci';
+import { CiSearch } from 'react-icons/ci';
 
 import './sandbox.styles.css';
 import { WorkflowRenderer } from '@app/(workspace)/sandbox/workflow-renderer';
@@ -35,24 +33,18 @@ const Sandbox = (): ReactNode => {
                             <SandboxController />
                             <SandboxRunButton />
                         </Panel>
-                        <Controls position="top-center"
-                            className="flow-controls"
-                            showZoom={false}
-                            showFitView={false}
-                            showInteractive={false}>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <LuMousePointer className="icon-button pointer-icon" />
-                            </ControlButton>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <IoHandRightOutline className="icon-button hand-icon" />
-                            </ControlButton>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <FiUserPlus className="icon-button add-agent-icon" strokeWidth={1.1} />
-                            </ControlButton>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <svg className="icon-button branches-icon"
-                                    strokeWidth="1.3"
-                                    viewBox="4 -4 26 26"
+                        <Panel className="flow-controls">
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <LuMousePointer className="icon-button" strokeWidth={1.5} />
+                            </button>
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <IoHandRightOutline className="icon-button" strokeWidth={1.5} />
+                            </button>
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <FiUserPlus className="icon-button" strokeWidth={1.5} />
+                            </button>
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <svg className="icon-button branches-icon" viewBox="4 -4 26 26"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <rect x="16.8995"
                                         y="1"
@@ -60,26 +52,36 @@ const Sandbox = (): ReactNode => {
                                         height="14"
                                         transform="rotate(45 16.8995 1)"
                                         stroke="currentColor"
+                                        strokeWidth="1.5"
                                         strokeLinecap="round"
                                         strokeLinejoin="round" />
-                                    <path d="M1 11L7 11" stroke="currentColor" strokeLinecap="round"
+                                    <path d="M1 11L7 11"
+                                        stroke="currentColor"
+                                        strokeWidth="1.5"
+                                        strokeLinecap="round"
                                         strokeLinejoin="round" />
-                                    <path d="M22 5.65686L26.2426 1.41422" stroke="currentColor" strokeLinecap="round"
+                                    <path d="M22 5.65686L26.2426 1.41422"
+                                        stroke="currentColor"
+                                        strokeWidth="1.5"
+                                        strokeLinecap="round"
                                         strokeLinejoin="round" />
-                                    <path d="M22 16L26.2426 20.2426" stroke="currentColor" strokeLinecap="round"
+                                    <path d="M22 16L26.2426 20.2426"
+                                        stroke="currentColor"
+                                        strokeWidth="1.5"
+                                        strokeLinecap="round"
                                         strokeLinejoin="round" />
                                 </svg>
-                            </ControlButton>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <PiClipboardLight className="icon-button" />
-                            </ControlButton>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <CiCircleCheck className="icon-button" />
-                            </ControlButton>
-                            <ControlButton onClick={() => alert('Something magical just happened. ✨')}>
-                                <CiSearch size={40} className="icon-button" />
-                            </ControlButton>
-                        </Controls>
+                            </button>
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <PiClipboardLight className="icon-button" strokeWidth={1.5} />
+                            </button>
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <CiCircleCheck className="icon-button" strokeWidth={0.5} />
+                            </button>
+                            <button onClick={() => alert('Something magical just happened. ✨')}>
+                                <CiSearch className="icon-button" strokeWidth={0.5} />
+                            </button>
+                        </Panel>
                         {/*<Controls position="top-center" showZoom={false} showFitView={false} showInteractive={false}*/}
                         {/*    className="search-button">*/}
                         {/*</Controls>*/}
