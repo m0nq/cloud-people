7311523e74555737df554d9a610571b71465cc00
Monk <forgetmenaught@gmail.com>
2024-12-20 11:46:41 -0800

    Added notes for future implementations

--- src/components/sandbox-nodes/delivery-node.tsx
+++ src/components/sandbox-nodes/delivery-node.tsx
@@ -8,6 +8,13 @@ interface DeliveryNodeProps {
     // Add any other props you need
 }
 
+/*
+* DeliveryNode will have 3 states to display between 2 svgs
+* Delivery Pending: base colors already set
+* Ready for Review: background: linear-gradient(89deg, #C1D6E7 14.21%, #E1E4EB 101.01%);
+*   svg fill color: #2D3742
+* Clicked State: background and svg fill of Ready for Review state, but use open-box-icon
+* */
 const DeliveryNode: React.FC<DeliveryNodeProps> = ({ id }) => {
     return (
         <div className="delivery-node" data-node-id={id}>
