4106824e94b296f9f777f50d196c1701d77becf0
Monk <forgetmenaught@gmail.com>
2024-12-21 10:36:55 -0800

    Establish initial column layout

--- src/components/modals/agent-details-modal.tsx
+++ src/components/modals/agent-details-modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import './modal.styles.css';
 
 interface AgentDetailsModalProps {
     onClose: () => void;
@@ -6,11 +6,19 @@ interface AgentDetailsModalProps {
     parentNodeId?: string;
 }
 
-export const AgentDetailsModal: React.FC<AgentDetailsModalProps> = ({ onClose, children, parentNodeId }) => {
+export const AgentDetailsModal: React.FC<AgentDetailsModalProps> = ({ onClose, children }) => {
     return (
-        <div className="agent-details-modal">
-            {children}
-            {/* Content will be added in subsequent steps */}
+        <div className="agent-details-modal h-full max-h-[90vh] p-4">
+            <div className="flex gap-4 h-full w-full">
+                {/* Left Column */}
+                <div className="flex-1 bg-[#111212] rounded-lg h-full overflow-auto">{/* Content for left column will go here */}</div>
+
+                {/* Right Column */}
+                <div className="flex flex-col items-start w-96 h-full px-[0.0625rem] bg-[#111212] rounded-lg gap-2 overflow-auto">
+                    {children}
+                    {/* Content for right column will go here */}
+                </div>
+            </div>
         </div>
     );
 };
