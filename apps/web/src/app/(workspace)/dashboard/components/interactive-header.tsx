491fb0fb18a6b6a9912619a21d267dc060e8feb1
m0nq <forgetmenaught@gmail.com>
2025-03-16 09:57:03 -0700

    Dashboard button styling

--- apps/web/src/app/(workspace)/dashboard/components/interactive-header.tsx
+++ apps/web/src/app/(workspace)/dashboard/components/interactive-header.tsx
@@ -4,29 +4,26 @@ import { FaArrowRight } from 'react-icons/fa';
 import Link from 'next/link';
 
 import '../dashboard.styles.css';
-import { Button } from '@components/utils/button/button';
 import { IoIosList } from 'react-icons/io';
 
 export const InteractiveHeader = () => {
     return (
         <div className="interactive-header-container">
             <div className="header-buttons">
-                <Link href="/sandbox"
-                    className="header-link">
+                <Link href="/sandbox" className="header-link">
                     Start New Project
                     <FaArrowRight size={16} />
                 </Link>
-                <Button size="md"
-                    icon={<IoIosList size={24} />}
+                <button className="header-button"
                     onClick={() => {
                         const allProjectsElement = document.getElementById('all-projects-section');
                         if (allProjectsElement) {
                             allProjectsElement.scrollIntoView({ behavior: 'smooth' });
                         }
-                    }}
-                    className="header-button">
+                    }}>
+                    <IoIosList size={24} />
                     All Projects
-                </Button>
+                </button>
             </div>
         </div>
     );
